38250a6f586144d6e68c810f2d768080d93dd8a6
Jenni Tolvanen <jenni95@gmail.com>
2016-09-17 20:16:19 +0300

    perusnäkymät luotu ja lisätty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,31 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('suunnitelmat/frontpage.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function note_list() {
+        View::make('suunnitelmat/note_list.html');
+    }
+    
+    public static function note_show() {
+        View::make('suunnitelmat/note_show.html');
+    }
+    
+    public static function note_create() {
+        View::make('suunnitelmat/note_create.html');
+    }
+    
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,25 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/note', function() {
+    HelloWorldController::note_list();
+});
+
+$routes->get('/note/1', function() {
+    HelloWorldController::note_show();
+});
+
+$routes->get('/note/new', function() {
+    HelloWorldController::note_create();
+});
+
+$routes->get('/note/login', function() {
+    HelloWorldController::login();
+});
