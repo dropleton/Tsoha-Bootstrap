333336bd7faded8cbb7dd9e73be94c5830c4dff1
Jenni Tolvanen <jenni95@gmail.com>
2016-10-15 14:36:46 +0300

    Kaikkien muistiinpanojen katselun lisäys, datan päivitys

--- app/controllers/note_controller.php
+++ app/controllers/note_controller.php
@@ -6,8 +6,7 @@ class NoteController extends BaseController {
         self::check_logged_in();
         $kayttaja_id = $_SESSION['user'];
         $notes = Note::all($kayttaja_id);
-        $luokat = Luokka::all($kayttaja_id);
-        View::make('note/note_list.html', array('notes' => $notes, 'luokat' => $luokat));
+        View::make('note/note_list.html', array('notes' => $notes));
     }
 
     public static function show($id) {
--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,6 +1,3 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
 INSERT INTO Kayttaja (kayttajatunnus, salasana) VALUES ('Jenni', 'Jenni123');
 INSERT INTO Kayttaja (kayttajatunnus, salasana) VALUES ('porkkana', 'peruna');
-INSERT INTO Muistiinpano (otsikko, kayttaja_id, sisalto, prioriteetti) VALUES ('Raja-arvojen läksyt', '1', 'Eipä tuolta tullut mitään', '!!');
-INSERT INTO Muistiinpano (otsikko, kayttaja_id, sisalto, prioriteetti) VALUES ('Ostoslista', '2', 'Maitoa ja leipää', '!');
-INSERT INTO Luokka (nimi) VALUES ('Kouluroinat');
